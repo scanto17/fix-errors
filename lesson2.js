65f9256e6f64f00ea1123c8c1127f8af62f1e393
scanto17 <cantosean@gmail.com>
2020-02-03 21:48:01 -0800

    lesson 2

--- lesson2.js
+++ lesson2.js
@@ -1,6 +1,8 @@
 //Should console.log all names in the name array
 
 var names = ["Alibaba", "Cassandra", "AJ"]
+
 for (var i = 0; i <= names.length; i++) {
     console.log("My Name is " + names[i])
+    
 }
